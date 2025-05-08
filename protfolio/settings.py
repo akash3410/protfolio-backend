678ff29dcb518c0df7fbfd66b862ad869ca1eaee
akash3410 <mdarifuzzaman982k@gmail.com>
2025-05-08 20:24:15 +0600

    Deploy

--- protfolio/settings.py
+++ protfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z^!c0*h^z9wtw5n!ia#c_xsf4ts%i103k0+0i+vn-l&si3x)sd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['akashsprotfolio.pythonanywhere.com']
 
 
 # Application definition
@@ -140,6 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
