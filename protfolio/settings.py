804c63bb2df39d8b4ddaeb3e05fb09a2b0086bf8
akash3410 <mdarifuzzaman982k@gmail.com>
2025-05-11 01:32:12 +0600

    setting

--- protfolio/settings.py
+++ protfolio/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+from decouple import config, UndefinedValueError
 import os
 import dj_database_url
 from pathlib import Path
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret-key")
+SECRET_KEY = os.environ.get("SECRET_KEY") or config("SECRET_KEY", default="fallback-key")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
